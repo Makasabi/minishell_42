ae5ceca3d0e2b2815ac25ff93e996cdc16a1892c
Marie RONY <mrony@made-f0Br6s9.clusters.42paris.fr>
2023-09-17 16:35:09 +0200

    cleaned make/

--- make/includes.mk
+++
@@ -1,9 +0,0 @@
-INCLUDES += ./inc/minishell.h
-INCLUDES += ./inc/colors.h
-INCLUDES += ./inc/exec.h
-INCLUDES += ./inc/env.h
-INCLUDES += ./inc/expand.h
-INCLUDES += ./inc/errors.h
-INCLUDES += ./inc/parsing.h
-INCLUDES += ./inc/heredoc.h
-INCLUDES += ./inc/builtins.h
